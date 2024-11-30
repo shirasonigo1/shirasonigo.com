e0048ef1bc4adfa84eb43f44ef9c6ede00fa714f
Shira Sonigo <shirasonigo1@gmail.com>
2024-11-30 18:19:40 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `shira-sonigo-site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
