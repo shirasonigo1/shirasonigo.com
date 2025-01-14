9c8975efd81ea4184a8602b2943304a0bbfeafb3
shirasonigo1 <shirasonigo1@gmail.com>
2025-01-14 19:59:13 +0000

    fixed for build

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,43 +1,14 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
-import { Link, graphql } from 'gatsby'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Coming soon!">
-      {/* {
-        data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-               <h2>
-              <Link to={`/blog/${node.frontmatter.slug}`}>
-                {node.frontmatter.title}
-              </Link>
-            </h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
-          </article>
-        ))
-      } */}
     </Layout>
   )
 }
 
-// export const query = graphql`
-//   query {
-//     allMdx(sort: { frontmatter: { date: DESC }}) {
-//       nodes {
-//         frontmatter {
-//           date(formatString: "MMMM D, YYYY")
-//           title
-//           slug
-//         }
-//         id
-//         excerpt
-//       }
-//     }
-//   }
-// `
 
 export const Head = () => <Seo title="My Blog Posts" />
 
