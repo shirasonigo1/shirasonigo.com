5d94e0492776841ccee104924ea8ab16162faf1e
Shira Sonigo <shirasonigo1>
2025-02-09 20:47:33 +0000

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,12 @@ const Header = () => {
         className="profileImage"
         width={50}
         height={50}
-        imgStyle={{ borderRadius: '50%', objectFit: "cover", objectPosition:"center bottom" }}
+        imgStyle={{                   borderRadius: '50%',
+          objectFit: "cover",
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          inset: "0", }}
       />
           </Link> )}
       <span>
